d80e9caecc49518ccb9fe03d500745db58846697
xabierlaiseca <xabier@laiseca.me>
2016-07-16 18:51:35 +0100

    support for running ansible without inventory

--- ansiblest/domain/service/ansible.py
+++ ansiblest/domain/service/ansible.py
@@ -1,6 +1,10 @@
 from subprocess import call
 
-def run_ansible_playbook(inventory_path, playbook_path, output_path):
+def run_ansible_playbook(playbook_path, output_path, inventory_path=None):
     with open(output_path, mode="wa") as output_file:
-        command = [ "ansible-playbook", "-i", inventory_path, playbook_path ]
+        command = [ "ansible-playbook", playbook_path ]
+
+        if inventory_path is not None:
+            command += [ "-i", inventory_path ]
+
         return call(command, stdout=output_file, stderr=output_file).returncode
--- test/test_ansiblest/domain/service/test_ansible.py
+++ test/test_ansiblest/domain/service/test_ansible.py
@@ -10,15 +10,33 @@ class TestRunAnsiblePlaybookFunction(TestCase):
 
     RETURN_CODE = 10
 
+
+
     @patch("ansiblest.domain.service.ansible.call")
     @patch("ansiblest.domain.service.ansible.open")
-    def test__run_ansible_playbook(self, open_mock, call_mock):
+    def test__run_ansible_playbook__without_inventory(self, open_mock, call_mock):
         open_mock.return_value.__enter__.return_value = output_file_mock = Mock()
         call_mock.return_value = FakeCallReturnValue(self.RETURN_CODE)
 
-        expected_cmd = [ "ansible-playbook", "-i", self.INVENTORY_PATH, self.PLAYBOOK_PATH ]
+        expected_cmd = [ "ansible-playbook", self.PLAYBOOK_PATH ]
 
-        return_code = run_ansible_playbook(self.INVENTORY_PATH, self.PLAYBOOK_PATH, self.OUTPUT_PATH)
+        return_code = run_ansible_playbook(self.PLAYBOOK_PATH, self.OUTPUT_PATH)
+
+        open_mock.assert_called_once_with(self.OUTPUT_PATH, mode="wa")
+        call_mock.assert_called_once_with(expected_cmd, stdout=output_file_mock, stderr=output_file_mock)
+
+        self.assertEqual(self.RETURN_CODE, return_code, msg="unexpected status code")
+
+
+    @patch("ansiblest.domain.service.ansible.call")
+    @patch("ansiblest.domain.service.ansible.open")
+    def test__run_ansible_playbook__with_inventory(self, open_mock, call_mock):
+        open_mock.return_value.__enter__.return_value = output_file_mock = Mock()
+        call_mock.return_value = FakeCallReturnValue(self.RETURN_CODE)
+
+        expected_cmd = [ "ansible-playbook", self.PLAYBOOK_PATH, "-i", self.INVENTORY_PATH ]
+
+        return_code = run_ansible_playbook(self.PLAYBOOK_PATH, self.OUTPUT_PATH, self.INVENTORY_PATH)
 
         open_mock.assert_called_once_with(self.OUTPUT_PATH, mode="wa")
         call_mock.assert_called_once_with(expected_cmd, stdout=output_file_mock, stderr=output_file_mock)
